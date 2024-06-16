c77336fcbd0c420c00f0f643befd1873cdf9b27a
korrova11 <korrova@yandex.ru>
2024-06-16 10:56:17 +0300

    testLess

--- src/main/java/com/gridnine/testing/CheckOnTwoHoursBetween.java
+++ src/main/java/com/gridnine/testing/CheckOnTwoHoursBetween.java
@@ -12,16 +12,18 @@ public class CheckOnTwoHoursBetween implements Filter {
     public List<Flight> filter(List<Flight> list) {
         List<Flight> flights = new ArrayList<>();
         boolean b;
+        int summa = 0;
         for (int i = 0; i < list.size(); i++) {
             b=true;
             ArrayList<Segment> listSegment = (ArrayList<Segment>) list.get(i).getSegments();
             for (int j = 0; j < listSegment.size() - 1; j++) {
-                if (ChronoUnit.HOURS.between(listSegment.get(j).getArrivalDate(),
-                        listSegment.get(j + 1).getDepartureDate()) > 2) {
-                    b = false; break;
-                }
+
+                summa= (int) (summa+ ChronoUnit.HOURS.between(listSegment.get(j).getArrivalDate(),
+                                        listSegment.get(j + 1).getDepartureDate()));
+
+
             }
-            if(b){ Flight flight = new Flight(listSegment);
+            if(summa<=2){ Flight flight = new Flight(listSegment);
                 flights.add(flight);
            }
         }
